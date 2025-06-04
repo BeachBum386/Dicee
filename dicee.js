0332ac470c48e13479fe38e05449a06d84f62f2c
BeachBum386 <167818398+BeachBum386@users.noreply.github.com>
2025-06-04 14:48:55 -0400

    Add files via upload

---
+++ dicee.js
@@ -0,0 +1,22 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage1 = "Dice" + randomNumber1 + ".png";
+var randomImageSource1 = "images/" + randomDiceImage1;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "Dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
+
+document.querySelector("h1").innerHTML = "It's a draw!";
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 wins!";
+}
+
+if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
+
