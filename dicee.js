29ad525ea552ad12be269be3e46a3e323a5e6651
BeachBum386 <167818398+BeachBum386@users.noreply.github.com>
2025-06-04 14:52:05 -0400

    Update dicee.js

--- dicee.js
+++ dicee.js
@@ -1,12 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage1 = "Dice" + randomNumber1 + ".png";
-var randomImageSource1 = "images/" + randomDiceImage1;
+var randomImageSource1 = randomDiceImage1;
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource1);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage2 = "Dice" + randomNumber2 + ".png";
-var randomImageSource2 = "images/" + randomDiceImage2;
+var randomImageSource2 = randomDiceImage2;
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSource2);
 
